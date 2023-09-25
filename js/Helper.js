44d95d51c37596a273389f8ee07f889bdf299218
yestai <igor.adbullin.95@gmail.com>
2023-09-25 17:30:44 +0300

    add prettier format (node.js)

--- js/Helper.js
+++ js/Helper.js
@@ -1,90 +1,74 @@
 window.onload = function () {
-    let canvasPrinter = new Graph();
+  let canvasPrinter = new Graph();
 
-    let findAndReturnSelectedRadius = () => {
-        let selected = document.querySelector("input[name='R']:checked");
-        return selected.value;
-    }
+  let findAndReturnSelectedRadius = () => {
+    let selected = document.querySelector("input[name='R']:checked");
+    return selected.value;
+  };
 
+  canvasPrinter.redrawAll(findAndReturnSelectedRadius());
+
+  document.getElementById("radio-radius").onchange = function () {
     canvasPrinter.redrawAll(findAndReturnSelectedRadius());
+    drawAllPoints(canvasPrinter);
+  };
 
-    document.getElementById("radio-radius")
-        .onchange = function () {
-        canvasPrinter.redrawAll(findAndReturnSelectedRadius());
-        drawAllPoints(canvasPrinter);
-    };
+  document.getElementById("checkButton").onclick = function () {
+    if (validateX() && validateY()) {
+      let x = document.querySelector(".x-select").value;
+      let y = document.getElementById("Y-input").value.replace(",", ".");
+      let r = findAndReturnSelectedRadius();
 
+      $.ajax({
+        type: "POST",
+        url: "php/Server.php",
+        data: { x: x, y: y, r: r },
+        success: function (serverAnswer) {
+          const jsonObject = JSON.parse(serverAnswer);
+          let table = document.querySelector("table");
+          let tBody = table.getElementsByTagName("tbody")[0];
+          let row = tBody.insertRow();
 
-    document.getElementById('checkButton').onclick = function () {
+          row.insertCell().textContent = jsonObject.response_time;
+          row.insertCell().textContent = jsonObject.current_time;
+          row.insertCell().textContent = jsonObject.boolean;
+          row.insertCell().textContent = jsonObject.x;
+          row.insertCell().textContent = jsonObject.y;
+          row.insertCell().textContent = jsonObject.r;
 
-        if (validateX() && validateY()) {
-
-            let x = document.querySelector('.x-select').value;
-            let y = document.getElementById("Y-input").value.replace(',', '.');
-            let r = findAndReturnSelectedRadius();
-
-            $.ajax({
-                type: 'POST',
-                url: 'php/Server.php',
-                data: {"x": x, "y": y, "r": r},
-                success: function (serverAnswer) {
-
-                    const jsonObject = JSON.parse(serverAnswer);
-                    let table = document.querySelector("table");
-                    let tBody = table.getElementsByTagName("tbody")[0];
-                    let row = tBody.insertRow();
-
-
-                    row.insertCell().textContent = jsonObject.response_time;
-                    row.insertCell().textContent = jsonObject.current_time;
-                    row.insertCell().textContent = jsonObject.boolean;
-                    row.insertCell().textContent = jsonObject.x;
-                    row.insertCell().textContent = jsonObject.y;
-                    row.insertCell().textContent = jsonObject.r;
-
-                    drawAllPoints(canvasPrinter);
-                }
-            });
-        }
+          drawAllPoints(canvasPrinter);
+        },
+      });
     }
+  };
 
-    document.getElementById('clearButton').onclick = function () {
-        deleteAllRows(canvasPrinter, findAndReturnSelectedRadius());
-    }
-
+  document.getElementById("clearButton").onclick = function () {
+    deleteAllRows(canvasPrinter, findAndReturnSelectedRadius());
+  };
 };
 
 function drawAllPoints(canvasPrinter) {
-    let table = document.querySelector('table');
-    let rows = table.rows;
+  let table = document.querySelector("table");
+  let rows = table.rows;
 
-    for (let i = 0; i < rows.length; i++) {
-        let row = rows[i];
+  for (let i = 0; i < rows.length; i++) {
+    let row = rows[i];
 
-        let boolean = row.cells[2].innerText;
-        let x = row.cells[3].innerText;
-        let y = row.cells[4].innerText;
-        let inRange;
+    let inRange = row.cells[2].innerText;
+    let x = row.cells[3].innerText;
+    let y = row.cells[4].innerText;
 
-        if (boolean === 'false') {
-            inRange = false;
-        } else if (boolean === 'true') {
-            inRange = true;
-        }
-
-        canvasPrinter.drawPoint(x, y, inRange);
-
-    }
-
+    canvasPrinter.drawPoint(x, y, Boolean(inRange));
+  }
 }
 
 function deleteAllRows(canvasPrinter, finder) {
-    let table = document.querySelector('table');
-    let rows = table.rows;
+  let table = document.querySelector("table");
+  let rows = table.rows;
 
-    for (let i = rows.length - 1; i > 0; i--) {
-        table.deleteRow(i);
-    }
+  for (let i = rows.length - 1; i > 0; i--) {
+    table.deleteRow(i);
+  }
 
-    canvasPrinter.redrawAll(finder);
+  canvasPrinter.redrawAll(finder);
 }
--- js/Validator.js
+++ js/Validator.js
@@ -1,21 +1,21 @@
 function validateX() {
-    const result = document.querySelector('.x-select').value;
-    if (result !== 'none') {
-        return true;
-    } else {
-        alert("Choose x element")
-        return false;
-    }
+  const result = document.querySelector(".x-select").value;
+  if (result !== "none") {
+    return true;
+  } else {
+    alert("Choose x element");
+    return false;
+  }
 }
 
 function validateY() {
-    let element = document.getElementById("Y-input");
-    let y = element.value.replace(',', '.');
-    let regex = new RegExp(/^-?(?:3(?:\.0+)?|[0-2](?:\.[0-9]+)?|\.[0-9]+)$/)
-    if (regex.test(y.toString())) {
-        return true;
-    } else {
-        alert("y is decimal, between -3 and 3")
-        return false;
-    }
+  let element = document.getElementById("Y-input");
+  let y = element.value.replace(",", ".");
+  let regex = new RegExp(/^-?(?:3(?:\.0+)?|[0-2](?:\.[0-9]+)?|\.[0-9]+)$/);
+  if (regex.test(y.toString())) {
+    return true;
+  } else {
+    alert("y is decimal, between -3 and 3");
+    return false;
+  }
 }
